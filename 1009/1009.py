17a1fcb008de5976649a92f0fbbedd28cadb45bb
TheRealMonkey <mariskalnins@protonmail.com>
2021-12-03 10:29:40 +0000

    added functions

--- 1009/1009.py
+++ 1009/1009.py
@@ -1,20 +1,4 @@
-
-
-def init():
-    age_in_years = float(input("Enter your age in years:"))
-    height_in_cm = float(input("Enter your height in cm:"))
-    weight_in_kg = float(input("Enter your wight in kg:"))
-    gender = input("enter your gender(m|f)").lower()
-    if gender == "m":
-        bmr = 88.362 + (13.397 * weight_in_kg) + \
-            (4.799 * height_in_cm) - (5.677 * age_in_years)
-    else:
-        bmr = 447.593 + (9.247 * weight_in_kg) + \
-            (3.098 * height_in_cm) - (4.330 * age_in_years)
-    bmi = weight_in_kg / ((height_in_cm*100) * (height_in_cm*100))
-    exercise_in_week = int(
-        input("Enter the number of times you exersice a week:"))
-
+def calcultae_clries_to_maintain(exercise_in_week, bmr):
     if exercise_in_week == 0:
         colories_to_maintain = bmr*1.2
     elif exercise_in_week <= 3:
@@ -25,16 +9,46 @@ def init():
         colories_to_maintain = bmr*1.725
     else:
         colories_to_maintain = bmr*1.9
+    return colories_to_maintain
+
+
+def caluculate_bmr(gender, weight_in_kg, age_in_years, height_in_cm):
+    if gender == "m":
+        bmr = 88.362 + (13.397 * weight_in_kg) + \
+            (4.799 * height_in_cm) - (5.677 * age_in_years)
+    else:
+        bmr = 447.593 + (9.247 * weight_in_kg) + \
+            (3.098 * height_in_cm) - (4.330 * age_in_years)
+    return bmr
+
+
+def calculate_target_bmi(bmi, target_bmi):
+    if bmi > target_bmi:
+        return(f"you need to decrease your bmi by {bmi-target_bmi}")
+    else:
+        return(f"you need to increase your bmi by {target_bmi-bmi}")
+
+
+def convert_to_dp(number, dp):
+    return round(number, -abs(len(list(str(number).split(".")[0]))-abs(sf)))
+
+
+def init():
+    age_in_years = float(input("Enter your age in years:"))
+    height_in_cm = float(input("Enter your height in cm:"))
+    weight_in_kg = float(input("Enter your wight in kg:"))
+    gender = input("enter your gender(m|f)").lower()
+    exercise_in_week = int(
+        input("Enter the number of times you exersice a week:"))
     target_bmi = float(input("Enter the BMI you would like to achieve:"))
 
-    if bmi > target_bmi:
-        print(f"you need to decrease your bmi by {bmi-target_bmi}")
-    else:
-        print(f"you need to increase your bmi by {target_bmi-bmi}")
+    bmr = caluculate_bmr(gender, weight_in_kg, age_in_years, height_in_cm)
+    bmi = weight_in_kg / ((height_in_cm*100) * (height_in_cm*100))
+    colories_to_maintain = calcultae_clries_to_maintain(exercise_in_week, bmr)
+    calculated_target_bmi = calculate_target_bmi(bmi, target_bmi)
 
-    print(f"BMI: {bmi}\nBMR:{bmr}")
+    print(f"BMI:{round(bmi,1)}\nBMR:{round(bmr,2)}\nCalories to maintain:{round(colories_to_maintain)}\n{calculated_target_bmi}")
 
 
 if __name__ == "__main__":
     init()
-    # print(list(str(bmi).split(".")[0]))
